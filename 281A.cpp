80652af2faa7b39b7394587e5f3dbf998be1193b
Pawan Chaudhary <pawanchaudhary2061@gmail.com>
2025-09-16 17:27:07 -0500

    problemsheet 281A

---
+++ 281A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (!s.empty()) {
+        s[0] = toupper(s[0]);
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
